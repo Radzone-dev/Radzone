71b0113c8a54082e0bf74406fe8f155043259e63
Radzone <radislav@free.fr>
2024-08-28 20:56:40 +0200

    up

--- Ajax/files/display-comment.php
+++
@@ -1,6 +0,0 @@
-<?php
-$name = htmlspecialchars($_POST["name"]);
-$comment = htmlspecialchars($_POST["comment"]);
-echo "Hi, $name. Your comment has been received successfully." . "<br>";
-echo "Here's the comment what you've entered: $comment";
-?>
